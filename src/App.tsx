420374204cd26097a25673a29c5c5c4439959f4e
Akash Jangra <aj.akashjangra@gmail.com>
2024-01-24 11:20:57 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='main h-screen w-screen'>
+      <h1 className='text-center'>Hello there 👋</h1>
+    </main>
+  )
+}
+
+export default App
